03268eb55191d2ad45277133d69261647d0d2d12
Srivatsan14 <36353051+Srivatsan14@users.noreply.github.com>
2018-02-11 10:42:52 +0530

    Create weather3.js

---
+++ weather3.js
@@ -0,0 +1,21 @@
+class Weather{
+	    constructor(city,state){
+	        this.apiKey='66c408701951d903';
+	        this.city=city;
+	        this.state= state;
+	    }
+	
+	   async getWeather() {
+	       const response = await fetch (`https://api.wunderground.com/api/${this.apiKey}/conditions/q/${this.state}/${this.city}.json`);
+	
+	       const responseData = await response.json();
+	
+	       return responseData.current_observation;
+	   }
+	
+	   changeLocation(city,state){
+	       this.city=city;
+	       this.state= state;
+	   }
+	}
+
